411bcc5f23455439a5a3f997d00102aa779d0f68
Alex Karunny <karunny.alex@gmail.com>
2020-03-10 20:13:25 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,42 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+    let arr1 = [];
+    let arr2 = [];
+    let str;
+    for (let i = 0; i < expr.length; i += 10) {
+        arr.push(expr.substr(i, 10));
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        str = "";
+        if (arr[i].substr(0, 2) == "**") {
+            arr1.push(" ");
+            continue;
+        }
+        for (let j = 0; j < arr[i].length; j += 2) {
+            if (arr[i].substr(j, 2) == "10") {
+                str = str + ".";
+                continue;
+            } else if (arr[i].substr(j, 2) == "11") {
+                str = str + "-";
+                continue;
+            } else if (arr[i].substr(j, 2) == "00") {
+                continue;
+            }
+        }
+        arr1.push(str);
+    }
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] in MORSE_TABLE) {
+            arr2.push(MORSE_TABLE[arr1[i]]);
+        } else if(arr1[i] == " ") {
+            arr2.push(" ");
+        }
+        
+    }
+    return arr2.join("");
 }
 
 module.exports = {
